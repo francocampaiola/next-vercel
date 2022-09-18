724641bfe5ce63f84719d27e52b90b1ffb198ff3
Franco Campaiola <francocampaiola@hotmail.com>
2022-09-18 20:28:53 -0300

    Add files to current proyect

---
+++ pages/_app.tsx
@@ -0,0 +1,27 @@
+import { AppProps } from 'next/app';
+import { NextPage } from 'next';
+import { ReactElement, ReactNode } from 'react';
+
+import '../styles/globals.css'
+
+type NextPageWithLayout = NextPage & {
+  getLayout?: (page: ReactElement) => ReactNode;
+}
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+}
+
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+
+  const getLayout = Component.getLayout || ((page) => page );
+
+  // return (
+  //   <>
+  //     <Component {...pageProps} />
+  //   </>
+  // )
+  return getLayout( <Component {...pageProps} /> )
+}
+
+export default MyApp
